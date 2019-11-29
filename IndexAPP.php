b49e2b223563e58cf4646d716d9eb40451733801
19ULmobDexep <58207830+19ULmobDexep@users.noreply.github.com>
2019-11-29 16:22:26 +0100

    Add files via upload

---
+++ IndexAPP.php
@@ -0,0 +1,19 @@
+<?php
+
+include 'HeaderAPP.html';
+
+?>
+
+<form action="indexAPP.php" method="POST">
+
+<h2>Bienvenue sur le site Application.com </h2>
+
+<p>Sur le site <italique>Application.com</italique> vous pourrez choisir des applications mobile de tout genre. 
+<br>Entrer vos coordonnées dans la section <strong> formulaire </strong> pour recevoir de nombreuses offres et être à jour sur les nouvelles applications mobile.<br>
+Bonne visite !</p>
+
+</form>
+
+<?php
+
+include 'FooterAPP.html';
